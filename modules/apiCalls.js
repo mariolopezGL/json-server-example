a4b4114e72ddbd150ba40186f71ddccbf193b120
mariolopezGL <mario.lopez@gorillalogic.com>
2020-11-17 16:45:00 -0600

    Delete apiCalls.js

--- modules/apiCalls.js
+++
@@ -1,9 +0,0 @@
-export function fetchPosts() {
-  // fetch('https://jsonplaceholder.typicode.com/posts').then(function (response) {
-  //   // The API call was successful!
-  //   console.log('success!', response);
-  // }).catch(function (err) {
-  //   // There was an error
-  //   console.warn('Something went wrong.', err);
-  // });
-}
